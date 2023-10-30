c3edcdaec914e5bd0946ea47aa44b7fdf111850a
Ydemae <113693952+Ydemae@users.noreply.github.com>
2023-10-30 14:19:54 +0100

    t

--- ActualTP/src/modele/conducteur.class.php
+++ ActualTP/src/modele/conducteur.class.php
@@ -1,29 +1,60 @@
 <?php
-    class Conducteur
+class Conducteur
+{
+	private $num_permis;
+	private $date_permis;
+	private $nom;
+	private $prenom;
+	private $mdp;
+
+	function __construct(string $num_permis = '', $date_permis = new date(), string $nom = '', string $prenom = '', string $mdp = '')
 	{
-		private $num_permis;
-		private $date_permis;
-		private $nom;
-		private $prenom;
-		private $mdp;
-
-		function __construct(string $num_permis='', date $date_permis= new date(), string $prenom='', string $login = '', string $mdp = '') {
-			$this->idpers		= $idpers;
-			$this->nom	= $nom;
-			$this->prenom	= $prenom;
-			$this->login	= $login;
-			$this->mdp	= $mdp;
-		}
-
-		function getIdpers		() : string			{ return $this->idpers; 		}
-		function setIdpers		(string $id)		{ $this->idpers = $id; 			}
-		function getNom	() : string			{ return $this->nom; 	} 
-		function setNom	(string $libelle)	{ $this->nom=$libelle; 	}		
-		function getPrenom	() : string			{ return $this->prenom; 		}
-		function setPrenom	(string $prenom)		{ $this->prenom=$prenom; 		}
-		function getLog		() : string			{ return $this->login; 		}
-		function setLog		(string $login)		{ $this->login = $login; 			}
-		function getMdp		() : string			{ return $this->mdp; 		}
-		function setMdp		(string $mdp)		{ $this->mdp = $mdp; 			}
+		$this->num_permis = $num_permis;
+		$this->date_permis = $date_permis;
+		$this->prenom = $prenom;
+		$this->nom = $nom;
+		$this->mdp = $mdp;
 	}
+
+	function getNum()
+	{
+		return $this->num_permis;
+	}
+	function setNum(int $num)
+	{
+		$this->num_permis = $num;
+	}
+	function getDatePermis()
+	{
+		return $this->date_permis;
+	}
+	function setDatePermis(string $date_permis)
+	{
+		$this->date_permis = $date_permis;
+	}
+	function getNom()
+	{
+		return $this->nom;
+	}
+	function setNom(string $nom)
+	{
+		$this->nom = $nom;
+	}
+	function getPrenom()
+	{
+		return $this->prenom;
+	}
+	function setPrenom(string $prenom)
+	{
+		$this->prenom = $prenom;
+	}
+	function getMdp()
+	{
+		return $this->mdp;
+	}
+	function setMdp(string $mdp)
+	{
+		$this->mdp = $mdp;
+	}
+}
 ?>
