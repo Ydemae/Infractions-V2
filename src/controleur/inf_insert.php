3ba33f5f4cc952ef65e65878d3a6c2bb7ffce990
Ydemae <113693952+Ydemae@users.noreply.github.com>
2023-11-15 12:09:54 +0100

    modif

--- src/controleur/inf_insert.php
+++ src/controleur/inf_insert.php
@@ -34,7 +34,7 @@ if (!file_exists("../../fichiers/infractions_ext.json")) {
             $fileError = "L'insertion n'a pas pu être effectuée, veuillez vérifier le format du fichier json.<br>Exemple de ligne correcte :" . '{"date_inf":"05/10/2023","num_immat":"AA643BB","num_permis": "AZ71","délits" :[1]}';
         }
         else if($errorCode == 2){
-            $fileError = "L'insertion n'a pas pu être effectuée car un id_delit présent dans le json n'existe pas dans la base";
+            $fileError = "Certaines infractions n'ont pas pu être insérées car un id_delit présent dans le json n'existe pas dans la base";
         } else {
             $successCode = "L'insetion a bien été effectuée, vous pouvez retourner à la liste des infractions";
         }
